480278e070bc7f256cc10fe90617d17ea424a495
Matt Coker <matt.coker@gmail.com>
2015-10-09 09:42:04 -0500

    Clean up variable declarations, break up text generation to be more modular and easier to modify in code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,16 +6,15 @@ chrome.extension.sendMessage({}, function(response) {
 
       function checkForEditBar() {
         var cS = document.querySelector('.clone_story'),
-        btn = document.createElement('button'),
-        ed = document.querySelector('.edit .controls'),
-        generateStory;
+            btn = document.createElement('button'),
+            ed = document.querySelector('.edit .controls');
 
         if (!ed) { return; }
         if ($(ed).find('.story_template').length) { return; }
 
         ed.style.width = '311px';
         btn.className = 'left_endcap hoverable story_template';
-        btn.title = "Apply description template to this story";
+        btn.title = 'Apply fdescription template to this story';
         btn.innerHTML = '<img src="//i.imgur.com/qvQ83UA.png">';
         cS.className = cS.className + ' capped';
         cS.insertAdjacentElement('beforebegin', btn);
@@ -28,17 +27,24 @@ chrome.extension.sendMessage({}, function(response) {
           var nextSection = $(btn).parents('.model_details').eq(0).next(),
               textArea = nextSection.find('.editor.tracker_markup.description'),
               existingData = textArea.val(),
-              ev = new jQuery.Event('keyup'),
-              markdown = "### Context\n---\n\n*Include background details for this story here*\n\n### How To Demo\n---\n\n1. *Start your demo here*\n\n### Implementation Details\n---\n\n*Include technical details or considerations here*\n\n### Design/UX Details\n---\n\n*Include design details or considerations here*\n\n";
+              ev = new jQuery.Event('keyup');
+
           ev.which = 13;
           ev.keyCode = 13;
+
+          var context    = '### Context\n---\n\n*Include background details for this story here*\n\n',
+              htd        = '### How To Demo\n---\n\n1. *Start your demo here*\n\n',
+              impDetails = '### Implementation Details\n---\n\n*Include technical details or considerations here*\n\n',
+              uxDetails  = '### Design/UX Details\n---\n\n*Include design details or considerations here*\n\n',
+              origContent = '\n\n### Original Content\n---\n\n' + existingData,
+              markdown = context + htd + impDetails + uxDetails;
+
           nextSection.find('.rendered_description').trigger('click');
           if (textArea.val()) {
             if (textArea.val() === markdown) {
-              alert("The description for this story already matches the current template.");
+              alert('The description for this story already matches the current template.');
             } else if (confirm('Existing description content will be placed at the bottom. Would you like to continue?')) {
-              markdown += "\n\n### Original Content\n---\n\n";
-              textArea.val(markdown + existingData)
+              textArea.val(markdown + origContent);
             }
           } else {
             textArea.val(markdown);
